d0c948552d07f4feb56ec179ae9693272c13e285
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-04-25 11:18:00 +0300

    debounced input one second

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
+// tslint:disable-next-line:import-blacklist
+import { Subject } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-search',
@@ -11,8 +13,15 @@ export class SearchComponent implements OnInit {
 
   public get ui() { return this.portfolioComponent.ui; }
 
+
+  searchTokenChanged: Subject<string> = new Subject<string>();
+
   constructor(
     public portfolioComponent: PortfolioComponent) {
+    this.searchTokenChanged
+      .debounceTime(1000) // wait 1 sec after the last event before emitting last event
+      .distinctUntilChanged() // only emit if value is different from previous value
+      .subscribe(_ => { this.searchToken = _; });
   }
 
   ngOnInit() {
@@ -23,5 +32,9 @@ export class SearchComponent implements OnInit {
   }
   @Input() set searchToken(value: string) {
     this.portfolioComponent.searchToken = value;
+  }
+
+  onFieldChange(query: string) {
+    this.searchTokenChanged.next(query);
   }
 }
