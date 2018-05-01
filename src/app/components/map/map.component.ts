685e117dd1f63fea39c266a1da5f0c54b1aec78c
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-05-01 18:59:50 +0300

    hooked up the map to the seach results

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 import * as Plotly from 'plotly.js';
 
 @Component({
@@ -9,61 +10,127 @@ import * as Plotly from 'plotly.js';
 export class MapComponent implements OnInit, AfterViewInit {
   @Input() key: any;
 
-  constructor() {
+  mapHTMLElement: HTMLDivElement;
+
+  @HostListener('window:resize') onResize() { this.resize(); }
+  @HostListener('window:beforeprint', ['$event']) onBeforePrint(event) { this.resize(); }
+
+  constructor(
+    public portfolioComponent: PortfolioComponent) {
+    portfolioComponent.searchTokenChanged.subscribe(_ => this.onSearchTokenChanged(_));
   }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    this.main();
+    this.drawMap('After view init');
   }
 
-  main() {
-    Plotly.d3.csv('../../assets/countries.csv',
-      function (err, rows) {
-        // tslint:disable-next-line:no-shadowed-variable
-        function unpack(rows, key) {
-          return rows.map(function (row) { return row[key]; });
-        }
+  private onSearchTokenChanged(value: string) {
+    this.drawMap('Search token changed');
+  }
 
-        const data = [{
-          type: 'choropleth',
-          locationmode: 'country names',
-          locations: unpack(rows, 'Country'),
-          z: unpack(rows, 'Weight'),
-          // text: unpack(rows, 'Country'),
-          autocolorscale: false,
-          colorscale: [
-            [0, 'rgb(65,105,225)'],
-            [1, 'rgb(220,166,224)']
-          ],
-          colorbar: {
-            // title: 'Projects',
-            thickness: 20
-          },
-          marker: {
-            line: {
-              color: 'rgb(220,220,220)',
-              width: 1
-            }
-          }
-        }];
+  private resize() {
+    if (this.mapHTMLElement) {
+      Plotly.Plots.resize(this.mapHTMLElement);
+    }
+  }
 
-        const layout = {
-          // title: 'Countries',
-          geo: {
-            projection: {
-              type: 'robinson'
-            },
-            scope: 'europe',
-            showlakes: true,
-            lakecolor: 'rgb(209,247,255)'
-          }
-        };
+  private async drawMap(drawMapCaller) {
+    // get map container
+    const mapContainer = document.getElementById('map');
+    if (!mapContainer) { return; }
 
-        const map = document.getElementById('map');
-        Plotly.plot(map, data, layout, { showLink: false });
-      });
+    const frequencies = this.getFrequenciesCache('Country');
+    const locations = frequencies.map(_ => _[0]);
+    const z = frequencies.map(_ => _[1].Count);
+
+    const data = [{
+      type: 'choropleth',
+      locationmode: 'country names',
+      locations: locations,
+      z: z,
+      // text: locations,
+      autocolorscale: false,
+      colorscale: [
+        [0, 'rgb(65,105,225)'],
+        [1, 'rgb(220,166,224)']
+        // [0, 'rgba(0, 0, 255, 1)'],
+        // [1, 'rgba(255, 0, 0, 1)']
+      ],
+      colorbar: {
+        // title: 'Markets count',
+        thickness: 20,
+        tick0: 0,
+        dtick: 1,
+        autotick: false
+      },
+      marker: {
+        line: {
+          color: 'rgba(0,0,0,0.5)',
+          width: 1
+        },
+        opacity: 1
+      }
+    }];
+
+    const layout = {
+      title: 'Markets',
+      geo: {
+        scope: 'world',
+        resolution: 50,
+
+        projection: {
+          type: 'robinson',
+          scale: 4.5
+        },
+        center: {
+          lon: 35,
+          lat: 52
+        },
+
+        bgcolor: 'rgba(0,0,0,0)',
+
+        showocean: true,
+        oceancolor: 'rgba(0,255,255,0.2)',
+        showland: false,
+        landcolor: 'rgba(0,0,0,0.2)',
+        showlakes: true,
+        lakecolor: 'rgba(0,128,255,0.2)',
+        showrivers: true,
+        rivercolor: 'rgba(0,0,255,0.2)',
+        showcountries: true,
+        countrycolor: 'rgba(128,128,128,0.2)',
+        showcoastlines: true,
+        coastlinecolor: 'rgba(0,0,0,0.2)',
+        showframe: true,
+        framecolor: 'rgba(0,0,0,0.2)',
+      },
+      paper_bgcolor: 'rgba(0,0,0,0)',
+      plot_bgcolor: 'rgba(0,0,0,0)',
+      margin: {
+        t: 0,
+        l: 0,
+        r: 0,
+        b: 0
+      }
+    };
+
+    // purge old map
+    mapContainer.innerHTML = '';
+
+    // crate new map dom node
+    const map = document.createElement('div');
+    map.style.cssText = 'width: 100%; height: 250px';
+    this.mapHTMLElement = mapContainer.appendChild(map);
+    console.log('Map width: ', this.mapHTMLElement.clientWidth);
+
+    // plot map
+    Plotly.plot(this.mapHTMLElement, data, layout, { showLink: false });
+  }
+
+  getFrequenciesCache(propertyName: string): any[] {
+    return this.portfolioComponent.getFrequenciesCache(propertyName);
   }
 }
