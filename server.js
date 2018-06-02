563c26f2e18782cb9389ca598dca69d57ccfe085
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-06-02 10:39:18 +0300

    temporarily disabled https redirection

--- server.js
+++ server.js
@@ -7,25 +7,26 @@ const compression = require('compression');
 app.use(compression());
 
 // Redirect http to https
-app.get('*', function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] != 'https' && !['localhost', '192.168.1.2'].includes(req.hostname)) {
-        var url = 'https://' + req.hostname;
-        // var port = app.get('port');
-        // if (port)
-        //   url += ":" + port;
-        url += req.url;
-        res.redirect(url);
-    }
-    else
-        next()
-});
+// app.get('*', function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] != 'https' && !['localhost', '192.168.1.2'].includes(req.hostname)) {
+//         var url = 'https://' + req.hostname;
+//         // var port = app.get('port');
+//         // if (port)
+//         //   url += ":" + port;
+//         url += req.url;
+//         res.redirect(url);
+//     }
+//     else
+//         next()
+// });
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 // Configure Express Rewrites
-app.get('/*', function(req, res) { 
-    res.sendFile(__dirname + '/index.html')
+app.all('/*', function (req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname });
 });
 
 // Start the app by listening on the default Heroku port
