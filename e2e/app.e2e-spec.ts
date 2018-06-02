ca917d2bf3eab5140f472195d544d11c06b10aea
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-06-02 22:46:32 +0300

    adjusted the e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('cv-generator-fe App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toContain('Curriculum Vitae');
   });
 });
