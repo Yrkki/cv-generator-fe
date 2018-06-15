f1f5b761a2e0b3b156b8a1feacaadcc948eb9c68
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-06-15 21:40:17 +0300

    Strong-typed and added identifiers to general timeline entries

--- src/app/services/general-timeline/general-timeline.service.ts
+++ src/app/services/general-timeline/general-timeline.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { GanttChartService } from '../gantt-chart/gantt-chart.service';
 import { Chart } from 'chart.js';
 
+import { GeneralTimelineEntry } from '../../classes/general-timeline-entry';
+
 /**
  * A general timeline chart diagram service.
  * @extends {@link GanttChartService}
@@ -30,21 +32,21 @@ export class GeneralTimelineService extends GanttChartService {
         fill: false,
         borderWidth: 0,
         pointRadius: 0,
-        data: this.items.map((_: any) => _.From)
+        data: this.items.map((_: GeneralTimelineEntry) => _.From)
       }, {
-        backgroundColor: this.items.map((_: any) =>
+        backgroundColor: this.items.map((_: GeneralTimelineEntry) =>
           this.filteredItems.some(__ => (__.Id === _.Id) && (__.Type === _.Type))
             ? _.Color
             : '#00000020'),
-        hoverBackgroundColor: this.items.map((_: any) => _.Color),
+        hoverBackgroundColor: this.items.map((_: GeneralTimelineEntry) => _.Color),
         borderColor: '#E8E8E8',
         hoverBorderColor: '#E8E8E8',
         fill: false,
         borderWidth: 1,
         pointRadius: 0,
-        data: this.items.map((_: any) => _.To - _.From)
+        data: this.items.map((_: GeneralTimelineEntry) => _.To - _.From)
       }],
-      labels: this.items.map((_: any) => _.Name)
+      labels: this.items.map((_: GeneralTimelineEntry) => _.Type.substr(0, 2) + _.Id + ': ' + _.Name)
     };
   }
 }
