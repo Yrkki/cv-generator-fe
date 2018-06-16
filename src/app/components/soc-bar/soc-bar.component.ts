1cf94c9c7738a91b4275dc4aef45cdd86b73af8d
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-06-16 12:17:50 +0300

    updated personal webpage

--- src/app/components/soc-bar/soc-bar.component.ts
+++ src/app/components/soc-bar/soc-bar.component.ts
@@ -9,6 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./soc-bar.component.scss']
 })
 export class SocBarComponent implements OnInit {
+  /** Socilal networks bar data */
+  private socBar = {
+    address: { title: 'Nadezhda 421 A 5 14, 1231 Sofia, Bulgaria', link: '' },
+    phone: { title: '+359893476810', link: 'tel:+359893476810' },
+    email: { title: 'yrkki@yahoo.com', link: 'mailto:yrkki@yahoo.com' },
+    linkedin: { title: 'georgimarinov', link: 'https://www.linkedin.com/in/georgimarinov' },
+    twitter: { title: 'yrkki', link: 'https://twitter.com/yrkki' },
+    facebook: { title: 'jorich', link: 'https://www.facebook.com/jorich' },
+    instagram: { title: 'jorich1', link: 'https://www.instagram.com/jorich1' }
+  };
 
   /**
    * Constructs the Socilal networks bar component.
