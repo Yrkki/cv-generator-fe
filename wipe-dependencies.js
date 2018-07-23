754f9ea5b62cd3e77223451c890d397b63a6245c
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-07-23 18:56:32 +0300

    updated dependencies

---
+++ wipe-dependencies.js
@@ -0,0 +1,23 @@
+const semVerRegEx = /\W+\d+.\d+.\d+-?((alpha|beta|rc)?.\d+)?/g
+const fs = require('fs')
+const wipeDependencies = () => {
+  const file = fs.readFileSync('package.json')
+  const content = JSON.parse(file)
+  for (const devDep in content.devDependencies) {
+    if (content.devDependencies[devDep].match(semVerRegEx)) {
+      content.devDependencies[devDep] = '*'
+    }
+  }
+  for (const dep in content.dependencies) {
+    if (content.devDependencies[devDep].match(semVerRegEx)) {
+      content.dependencies[dep] = '*'
+    }
+  }
+  fs.writeFileSync('package.json', JSON.stringify(content))
+}
+
+if (require.main === module) {
+  wipeDependencies()
+} else {
+  module.exports = wipeDependencies
+}
