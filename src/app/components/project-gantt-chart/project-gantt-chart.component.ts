e3a1bd9f4e187ade430853c98c63a062c08f7bf7
Jorich <35774231+Yrkki@users.noreply.github.com>
2018-10-31 18:19:34 +0200

    Fixed a template  property access instance

--- src/app/components/project-gantt-chart/project-gantt-chart.component.ts
+++ src/app/components/project-gantt-chart/project-gantt-chart.component.ts
@@ -10,5 +10,5 @@ import { Component } from '@angular/core';
 })
 export class ProjectGanttChartComponent {
   /** The component key */
-  protected key = 'Project Gantt';
+  public key = 'Project Gantt';
 }
