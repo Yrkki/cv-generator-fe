35b3c083cc47847cc059a3eb4a826828a49bdb64
Jorich <35774231+Yrkki@users.noreply.github.com>
2020-06-09 21:40:47 +0300

    Added e2e testing to the AppVeyor pipeline

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,18 +2,26 @@ import { AppPage } from './app.po';
 
 describe('cv-generator-fe App', () => {
   let page: AppPage;
+  let originalTimeout: number;
 
   beforeEach(() => {
     page = new AppPage();
+
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 2 * 60 * 1000; // default 5000
   });
 
-  it('should display welcome message', () => {
+  afterEach(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
+
+  it('should display Curriculum Vitae', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toContain('Curriculum Vitae');
   });
 
-  it('should display name', async () => {
+  it('should display name', () => {
     page.navigateToWebpage();
-    expect((await page.getNameText()).length).toBeGreaterThan(0);
+    expect(page.getParagraphText()).toContain('Georgi Marinov');
   });
 });
