44308be0f6a22e7b0d1ae0a616c5286cb1b0c345
Jorich <35774231+Yrkki@users.noreply.github.com>
2020-06-10 17:59:23 +0300

    Updated Karma config; Tried capture Travis display

--- karma.conf.js
+++ karma.conf.js
@@ -17,8 +17,8 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
       thresholds: {
         statements: 80,
@@ -67,6 +67,7 @@ module.exports = function (config) {
     },
 
     singleRun: false,
+    restartOnFileChange: true,
 
     browserDisconnectTimeout: 60 * 1000, // default 2000
     browserNoActivityTimeout: 4 * 60 * 1000, //default 10000
