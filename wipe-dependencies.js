d08d971b24b4fba7507120ed40aca1acd8c25042
Jorich <35774231+Yrkki@users.noreply.github.com>
2020-06-14 00:13:08 +0300

    Updated to a working version

--- wipe-dependencies.js
+++ wipe-dependencies.js
@@ -1,23 +1,17 @@
-const semVerRegEx = /\W+\d+.\d+.\d+-?((alpha|beta|rc)?.\d+)?/g
-const fs = require('fs')
-const wipeDependencies = () => {
-  const file = fs.readFileSync('package.json')
-  const content = JSON.parse(file)
-  for (const devDep in content.devDependencies) {
-    if (content.devDependencies[devDep].match(semVerRegEx)) {
-      content.devDependencies[devDep] = '*'
+var fs = require('fs'),
+  wipeDependencies = function() {
+    var file  = fs.readFileSync('package.json'),
+      content = JSON.parse(file);
+    for (var devDep in content.devDependencies) {
+      content.devDependencies[devDep] = '*';
     }
-  }
-  for (const dep in content.dependencies) {
-    if (content.devDependencies[devDep].match(semVerRegEx)) {
-      content.dependencies[dep] = '*'
+    for (var dep in content.dependencies) {
+      content.dependencies[dep] = '*';
     }
-  }
-  fs.writeFileSync('package.json', JSON.stringify(content))
-}
-
+    fs.writeFileSync('package.json', JSON.stringify(content,null,2));
+  };
 if (require.main === module) {
-  wipeDependencies()
+  wipeDependencies();
 } else {
-  module.exports = wipeDependencies
+  module.exports = wipeDependencies;
 }
