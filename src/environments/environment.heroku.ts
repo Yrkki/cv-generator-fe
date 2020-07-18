0ee44a77cd7529ae3a88efe1e61195390fc1aaa4
Georgi Marinov <yrkki@yahoo.com>
2020-07-18 18:55:45 +0300

    Added docker auth

--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -5,10 +5,12 @@ export const environment = {
   hosts: ['localhost', '192.168.1.2', '192.168.1.6', '192.168.99.100'],
   CV_GENERATOR_APPVEYOR_TOKEN: '',
   CV_GENERATOR_SKIP_REDIRECT_TO_HTTPS: 'false',
+  CV_GENERATOR_DOCKER_USERNAME: '',
+  CV_GENERATOR_DOCKER_TOKEN: '',
 
   CHROME_BIN: '/app/.apt/opt/google/chrome/chrome',
 
-  CI: 'true',
-  HEROKU: 'true',
+  CI: true,
+  HEROKU: true,
   CV_GENERATOR_AUDITING: false
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,11 @@ export const environment = {
   hosts: ['localhost', '192.168.1.2', '192.168.1.6', '192.168.99.100'],
   CV_GENERATOR_APPVEYOR_TOKEN: '',
   CV_GENERATOR_SKIP_REDIRECT_TO_HTTPS: '',
+  CV_GENERATOR_DOCKER_USERNAME: '',
+  CV_GENERATOR_DOCKER_TOKEN: '',
 
   CHROME_BIN: 'google-chrome',
 
-  CI: 'false',
+  CI: false,
   CV_GENERATOR_AUDITING: false
 };
