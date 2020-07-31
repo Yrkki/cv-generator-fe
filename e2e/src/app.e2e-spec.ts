b793d96c1aba31fb72e6617d076fffe3533d52f7
Georgi Marinov <yrkki@yahoo.com>
2020-07-31 12:06:00 +0300

    refactor(layout): Adjust tables and accessability

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -157,10 +157,10 @@ describe('workspace-project App', () => {
   // [% e2e-test-placeholder %]
 
   afterEach(async () => {
-    // // Assert that there are no errors emitted from the browser
-    // const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    // expect(logs).not.toContain(jasmine.objectContaining({
-    //   level: logging.Level.SEVERE,
-    // } as logging.Entry));
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
   });
 });
