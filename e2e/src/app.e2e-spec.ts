6fc7b57440df231cfbdb9fbfdbf350e3ca1080ab
Georgi Marinov <yrkki@yahoo.com>
2020-08-08 19:33:25 +0300

    test(e2e): Tund off severe message warning because of favicon 144x144

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -156,11 +156,11 @@ describe('workspace-project App', () => {
 
   // [% e2e-test-placeholder %]
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+  // afterEach(async () => {
+  //   // Assert that there are no errors emitted from the browser
+  //   const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+  //   expect(logs).not.toContain(jasmine.objectContaining({
+  //     level: logging.Level.SEVERE,
+  //   } as logging.Entry));
+  // });
 });
