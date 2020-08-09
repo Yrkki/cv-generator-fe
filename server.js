583ab0189b2145f97ca3cd09766fa8732efbc949
Georgi Marinov <yrkki@yahoo.com>
2020-08-09 17:15:58 +0300

    refactor(http2): Prepare for server for http2

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //Install new relic monitoring
 require('newrelic');
 
-//Install express server
+// Configure port
+const port = process.env.PORT || 5000
+
+// Install express server
 const express = require('express');
 const app = express();
 const compression = require('compression');
@@ -22,19 +25,19 @@ app.use(compression());
 
 // Redirect http to https
 app.get('*', function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https' &&
-        !(['true', 'TRUE'].includes(process.env.CV_GENERATOR_SKIP_REDIRECT_TO_HTTPS || '') ||
-            ['localhost', '192.168.1.2', '192.168.1.6', '192.168.99.100'].includes(req.hostname))) {
+  if (req.headers['x-forwarded-proto'] !== 'https' &&
+    !(['true', 'TRUE'].includes(process.env.CV_GENERATOR_SKIP_REDIRECT_TO_HTTPS || '') ||
+      ['localhost', '192.168.1.2', '192.168.1.6', '192.168.99.100'].includes(req.hostname))) {
 
-        var url = 'https://' + req.hostname;
-        // var port = app.get('port');
-        // if (port)
-        //   url += ":" + port;
-        url += req.url;
-        res.redirect(url);
-    }
-    else
-        next();
+    var url = 'https://' + req.hostname;
+    // var port = app.get('port');
+    // if (port)
+    //   url += ":" + port;
+    url += req.url;
+    res.redirect(url);
+  }
+  else
+    next();
 });
 
 // calc the root path
@@ -45,9 +48,34 @@ app.use(express.static(root));
 
 // Configure Express Rewrites
 app.all('/*', function (req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: root });
+  // Just send the index.html for other files to support HTML5Mode
+  res.sendFile('index.html', { root: root });
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+function welcome(error) {
+  if (error) {
+    console.error(error)
+    return process.exit(1)
+  } else {
+    console.log('Listening on port: ' + port + '.')
+  }
+}
+
+// Start the app by listening on the default port
+app.listen(port, welcome);
+
+// // Install http/2
+// const spdy = require('spdy')
+// const fs = require('fs');
+
+// // Prepare http/2 options
+// const spdy_options = {
+//   // key: fs.readFileSync(__dirname + '/openssl.key'),
+//   // cert: fs.readFileSync(__dirname + '/openssl.crt')
+//   cert: fs.readFileSync(__dirname + '/cv-generator-fe.cer')
+// }
+
+// // Serve http/2
+// spdy
+//   .createServer(spdy_options, app)
+//   .listen(port, welcome);
