b9eee8fea1b8bd030c766f90034a34ac3c1750f2
Jorich <35774231+Yrkki@users.noreply.github.com>
2020-11-15 15:03:12 +0200

    ci(datadog): base Datadog Agent activity on New Relic activity state

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 # Disable the Datadog Agent based on dyno type
-# if [ "$DYNOTYPE" == "run" ]; then
-  DISABLE_DATADOG_AGENT="true"
-# fi
+# # if [ "$DYNOTYPE" == "run" ]; then
+#   DISABLE_DATADOG_AGENT="false"
+# # fi
+
+# Base Datadog Agent activity on New Relic activity state
+DISABLE_DATADOG_AGENT="$NEW_RELIC_ENABLED"
+echo [ NEW_RELIC_ENABLED: $NEW_RELIC_ENABLED ]
+echo [ DISABLE_DATADOG_AGENT: $DISABLE_DATADOG_AGENT ]
 
 # # Set app version based on HEROKU_SLUG_COMMIT
 # if [ -n "$HEROKU_SLUG_COMMIT" ]; then
