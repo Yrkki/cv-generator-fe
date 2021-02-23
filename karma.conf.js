c53f45df5938f1c153d14c51a80c8dd55e2324a8
Jorich <35774231+Yrkki@users.noreply.github.com>
2021-02-23 14:28:36 +0200

    ci(angular): update angular

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,16 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: pluginsConfig(),
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: coverageIstanbulReporterConfig(),
     angularCli: {
