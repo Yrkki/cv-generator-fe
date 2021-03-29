fef02f9a43dd8a309a9c1ac16c95b4835cf16afe
Jorich <35774231+Yrkki@users.noreply.github.com>
2021-03-29 13:18:46 +0300

    ci(monitoring): enable datadog agent

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
+# # Set app version based on HEROKU_SLUG_COMMIT
+# if [ -n "$HEROKU_SLUG_COMMIT" ]; then
+#   DD_VERSION=$HEROKU_SLUG_COMMIT
+# fi
+
 # Disable the Datadog Agent based on dyno type
 # # if [ "$DYNOTYPE" == "run" ]; then
 #   DISABLE_DATADOG_AGENT="false"
 # # fi
 
-# Base Datadog Agent activity on New Relic activity state
-if [ "$NEW_RELIC_ENABLED" == true ]; then
-  unset DISABLE_DATADOG_AGENT
-else
-  DISABLE_DATADOG_AGENT="true"
-fi
-echo [ NEW_RELIC_ENABLED: $NEW_RELIC_ENABLED ]
-echo [ DISABLE_DATADOG_AGENT: $DISABLE_DATADOG_AGENT ]
-
-# # Set app version based on HEROKU_SLUG_COMMIT
-# if [ -n "$HEROKU_SLUG_COMMIT" ]; then
-#   DD_VERSION=$HEROKU_SLUG_COMMIT
+# # Base Datadog Agent activity on New Relic activity state
+# if [ "$NEW_RELIC_ENABLED" == true ]; then
+#   unset DISABLE_DATADOG_AGENT
+# else
+#   DISABLE_DATADOG_AGENT="true"
 # fi
+# echo [ NEW_RELIC_ENABLED: $NEW_RELIC_ENABLED ]
+
+# Enable Datadog Agent
+unset DISABLE_DATADOG_AGENT
+
+# Report
+echo [ DISABLE_DATADOG_AGENT: $DISABLE_DATADOG_AGENT ]
