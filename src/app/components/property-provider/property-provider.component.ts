b555f34241b1e2a6a40e985585249b25160ce112
Jorich <35774231+Yrkki@users.noreply.github.com>
2021-10-16 22:59:14 +0300

    ci(license): change the copyright notice

--- src/app/components/property-provider/property-provider.component.ts
+++ src/app/components/property-provider/property-provider.component.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2018 Georgi Marinov
+// Copyright (c) 2018 Georgi Marinov
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
