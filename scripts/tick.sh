9c658e442d62e6ed367f87414f0138e08f4f3585
Jorich <35774231+Yrkki@users.noreply.github.com>
2023-03-01 21:54:37 +0200

    refactor(tick): enhance tick script with 4 kinds

--- scripts/tick.sh
+++ scripts/tick.sh
@@ -1,14 +1,81 @@
 #!/bin/bash
 
-minutes=$1
+minutes="$1"
+if [ ! "$minutes" ]; then
+  minutes=1
+fi
 
-message=$'\033[1;30m'$'\033[1;34m'"%1s"$'\033[1;30m'" %4d:%02d minute(s) to go...\r"$'\033[0m'
+kind="$2"
+if [ ! "$kind" ]; then
+  kind=9
+fi
+
+message=$'\033[?25l\033[1;34m'"%1s"$'\033[1;30m'" %4d:%02d.%03d second(s) to go...\r"$'\033[0m'
 for ((i = $minutes - 1; i >= 0; i--)); do
+  case "$kind" in
+  1)
     for ((seconds = 59; seconds >= 0; seconds--)); do
-        sleep 0.5
-        printf "$message" "▀" "$i" "$seconds"
-        sleep 0.5
-        printf "$message" "▄" "$i" "$seconds"
+      sleep 0.25
+      printf "$message" "▄" "$i" "$seconds" "750"
+      sleep 0.25
+      printf "$message" "▄" "$i" "$seconds" "500"
+      sleep 0.25
+      printf "$message" "▀" "$i" "$seconds" "250"
+      sleep 0.25
+      printf "$message" "▀" "$i" "$seconds" "000"
     done
+    ;;
+
+  2)
+    for ((seconds = 59; seconds >= 0; seconds--)); do
+      sleep 0.25
+      printf "$message" "▄▄" "$i" "$seconds" "750"
+      sleep 0.25
+      printf "$message" "▄▄" "$i" "$seconds" "500"
+      sleep 0.25
+      printf "$message" "▀▀" "$i" "$seconds" "250"
+      sleep 0.25
+      printf "$message" "▀▀" "$i" "$seconds" "000"
+    done
+    ;;
+
+  10)
+    for ((seconds = 59; seconds >= 0; seconds--)); do
+      sleep 0.13
+      printf "$message" "◡" "$i" "$seconds" "875"
+      sleep 0.25
+      printf "$message" "◟" "$i" "$seconds" "625"
+      sleep 0.12
+      printf "$message" "◜" "$i" "$seconds" "500"
+      sleep 0.13
+      printf "$message" "◠" "$i" "$seconds" "375"
+      sleep 0.25
+      printf "$message" "◝" "$i" "$seconds" "125"
+      sleep 0.12
+      printf "$message" "◞" "$i" "$seconds" "000"
+    done
+    ;;
+
+  *)
+    for ((seconds = 59; seconds >= 0; seconds--)); do
+      sleep 0.125
+      printf "$message" "|" "$i" "$seconds" "875"
+      sleep 0.125
+      printf "$message" "/" "$i" "$seconds" "750"
+      sleep 0.125
+      printf "$message" "—" "$i" "$seconds" "625"
+      sleep 0.125
+      printf "$message" "\\" "$i" "$seconds" "500"
+      sleep 0.125
+      printf "$message" "|" "$i" "$seconds" "375"
+      sleep 0.125
+      printf "$message" "/" "$i" "$seconds" "250"
+      sleep 0.125
+      printf "$message" "—" "$i" "$seconds" "125"
+      sleep 0.125
+      printf "$message" "\\" "$i" "$seconds" "000"
+    done
+    ;;
+  esac
 done
-echo "                                                      "
+echo "                                                      "$'\033[?25h'$'\033[0m'
