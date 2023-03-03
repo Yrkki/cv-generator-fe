f76b4f345b754350c96f360c52c14a9be83d7cd6
Jorich <35774231+Yrkki@users.noreply.github.com>
2023-03-03 19:44:33 +0200

    feat(tick): add braille

--- scripts/tick.sh
+++ scripts/tick.sh
@@ -7,10 +7,10 @@ fi
 
 kind="$2"
 if [ ! "$kind" ]; then
-  kind=9
+  kind=10
 fi
 
-message=$'\033[?25l\033[1;34m'"%1s"$'\033[1;30m'" %4d:%02d.%03d second(s) to go...\r"$'\033[0m'
+message=$'\033[?25l\033[1;34m'"%1s"$'\033[1;30m'" %4d:%02d.%03d minutes(s) to go...\r"$'\033[0m'
 for ((i = $minutes - 1; i >= 0; i--)); do
   case "$kind" in
   1)
@@ -39,7 +39,7 @@ for ((i = $minutes - 1; i >= 0; i--)); do
     done
     ;;
 
-  10)
+  5)
     for ((seconds = 59; seconds >= 0; seconds--)); do
       sleep 0.13
       printf "$message" "◡" "$i" "$seconds" "875"
@@ -53,6 +53,48 @@ for ((i = $minutes - 1; i >= 0; i--)); do
       printf "$message" "◝" "$i" "$seconds" "125"
       sleep 0.12
       printf "$message" "◞" "$i" "$seconds" "000"
+    done
+    ;;
+
+  9)
+    for ((seconds = 59; seconds >= 0; seconds--)); do
+      sleep 0.13
+      printf "$message" "⠇" "$i" "$seconds" "875"
+      sleep 0.25
+      printf "$message" "⠋" "$i" "$seconds" "625"
+      sleep 0.12
+      printf "$message" "⠙" "$i" "$seconds" "500"
+      sleep 0.13
+      printf "$message" "⠸" "$i" "$seconds" "375"
+      sleep 0.25
+      printf "$message" "⠴" "$i" "$seconds" "125"
+      sleep 0.12
+      printf "$message" "⠦" "$i" "$seconds" "000"
+    done
+    ;;
+
+  10)
+    for ((seconds = 59; seconds >= 0; seconds--)); do
+      sleep 0.1
+      printf "$message" "⠇" "$i" "$seconds" "900"
+      sleep 0.1
+      printf "$message" "⠏" "$i" "$seconds" "800"
+      sleep 0.1
+      printf "$message" "⠋" "$i" "$seconds" "700"
+      sleep 0.1
+      printf "$message" "⠙" "$i" "$seconds" "600"
+      sleep 0.1
+      printf "$message" "⠹" "$i" "$seconds" "500"
+      sleep 0.1
+      printf "$message" "⠸" "$i" "$seconds" "400"
+      sleep 0.1
+      printf "$message" "⠼" "$i" "$seconds" "300"
+      sleep 0.1
+      printf "$message" "⠴" "$i" "$seconds" "200"
+      sleep 0.1
+      printf "$message" "⠦" "$i" "$seconds" "100"
+      sleep 0.1
+      printf "$message" "⠧" "$i" "$seconds" "000"
     done
     ;;
 
