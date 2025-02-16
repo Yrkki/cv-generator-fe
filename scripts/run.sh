6c6a2fe604f1bd9643032efbdbbe392d9ca5b05b
Jorich <35774231+Yrkki@users.noreply.github.com>
2025-02-16 16:55:20 +0200

    feat(run): reconfigure the script to wait LS_SECONDS before opening page

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+defaultSeconds=5
+if [ -z "${LS_SECONDS}" ]; then LS_SECONDS=$defaultSeconds; fi
+
 defaultPort=80
-if [ -z "${PORT}" ]; then PORT=$defaultPort; fi
-(sleep 3 && explorer "http://localhost":$PORT) &
-docker run -p $PORT:5000 jorich/cv-generator-fe
+if [ -z "${LS_PORT}" ]; then LS_PORT=$defaultPort; fi
+
+echo wait $LS_SECONDS seconds
+(sleep $LS_SECONDS && explorer "http://localhost":$LS_PORT) &
+
+docker run -p $LS_PORT:5000 jorich/cv-generator-fe
+
+# read  -n 1 -p "x" input
